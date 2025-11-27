735c57145093cfd6ed493b53770a2e1756052467
Afrasyab3 <afrasyabasif0@gmail.com>
2025-11-27 13:17:35 +0500

    Afrasyab Added Calculator Function

---
+++ file.py
@@ -0,0 +1,13 @@
+def calculator(num1, num2, operator):
+    if operator == '+':
+        return num1 + num2
+    elif operator == '-':
+        return num1 - num2
+    elif operator == '*':
+        return num1 * num2
+    elif operator == '/':
+        if num2 == 0:
+            return "Error: Division by zero!"
+        return num2 / num1
+    else:
+        return "Invalid operator! Use +, -, *, or /"
